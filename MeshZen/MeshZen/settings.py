607c6263520111bf63480862222750787682cebb
kchemorion <kchemorion@gmail.com>
2023-03-07 11:48:25 +0100

    final approach

--- MeshZen/MeshZen/settings.py
+++ MeshZen/MeshZen/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'meshes',
     'users',
     'visualizations',
-    'fontawesomefree',
     
 ]
 
@@ -132,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
