8ce0a77ab1a2540c5b147e6fc26c1d53d6a73cdf
kchemorion <kchemorion@gmail.com>
2023-03-07 06:08:55 +0100

    thinking about the search

--- MeshZen/base/urls.py
+++ MeshZen/base/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'base'
 
 urlpatterns = [
-    path('home', views.home, name='home'),
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('documentation/', views.documentation, name='documentation'),
     path('contact/', views.contact, name='contact'),
     path('features/', views.features, name='features'),
-    path('download/', views.contact, name='download'),
+    path('download/', views.download, name='download'),
 ]
