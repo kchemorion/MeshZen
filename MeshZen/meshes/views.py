c15f798ccf121c08ea723dc9be7501c83b2fa6fe
kchemorion <kchemorion@gmail.com>
2023-03-14 00:52:14 +0100

    okay i'm stopping now

--- MeshZen/meshes/views.py
+++ MeshZen/meshes/views.py
@@ -9,6 +9,7 @@ import base64
 from io import BytesIO
 from django.http import HttpResponse
 from django.shortcuts import render
+import pymeshlab as ml
 
 def results(request):
     # retrieve data from database or other source
@@ -58,9 +59,35 @@ class MeshSearchView(ListView):
             return mesh_files
         return MeshFile.objects.none()
 
+
+
 def visualize(request):
+    # Load the mesh using pymeshlab
+    mesh_path = os.path.join(settings.STATICFILES_DIRS[0], 'meshes/Vertebra.stl')
+    ms = ml.MeshSet()
+    ms.load_new_mesh(mesh_path)
+    mesh = ms.current_mesh()
+
+    # Simplify the mesh using pymeshlab
+    ms2 = ml.MeshSet()
+    ms2.add_mesh(mesh)
+    ms2.apply_filter('simplification_quadric_edge_collapse_decimation', targetfacenum=1000)
+    simplified_mesh = ms2.current_mesh()
+
+    # Write the mesh to an STL file
+    stl_path = os.path.join(settings.MEDIA_ROOT, 'meshes/simplified_mesh.stl')
+    ms2.save_current_mesh(stl_path)
+
+    # Read the contents of the STL file as a binary string
+    with open(stl_path, 'rb') as f:
+        stl_contents = f.read()
+    base64_stl = base64.b64encode(stl_contents).decode('utf-8')
+
+    # Generate context data for the template
     context = {
         'STATIC_URL': settings.STATIC_URL,
+        'mesh_url': 'data:application/stl;base64,' + base64_stl
     }
     return render(request, 'meshes/oops.html', context=context)
+
 
