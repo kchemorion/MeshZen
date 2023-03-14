5bd2118b0329923c1f3342ce84663b3e721cbabf
kchemorion <kchemorion@gmail.com>
2023-03-14 03:46:23 +0100

    server changes

--- meshes/urls.py
+++ meshes/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import MeshSearchView
 from . import views
+from django.shortcuts import render
 
 app_name = 'meshes'
 
 urlpatterns = [
     path('mesh_search/', MeshSearchView.as_view(), name='mesh_search'),
         path('results/', views.results, name='results'),
-        path('visualize/', views.render_mesh, name='render_mesh'),
+        path('visualize/', views.visualize, name='visualize'),
         path('report/', views.report, name='report'),
 ]
